842ab1d80ed3d4f1f39eb94a101d919bda39ce1e
Egor Ulybin <egorulybin@gmail.com>
2025-09-22 19:19:18 +0000

    finished register and login routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,18 +5,47 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+import bcrypt
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if User.query.filter_by(email=email).first():
+        return jsonify({"msg": "This user is already registered."})
 
-    return jsonify(response_body), 200
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(
+        password=password.encode("utf-8"), salt=salt)
+
+    new_user = User(email=email, password=hashed_password.decode(
+        "utf-8"), is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify(new_user.serialize()), 200
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        return jsonify({"msg": "No user found."}), 401
+
+    if not bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
+        return jsonify({"msg": "Invalid password"}), 401
+
+    token = create_access_token(identity=user.id)
+    return jsonify({"token": token, "user_id": user.id}), 200
